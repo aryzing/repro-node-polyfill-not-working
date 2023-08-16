2d37eef348d494b9f336847828f0430765a28a3b
Eduard Bardají Puig <ebardaji@gmail.com>
2023-08-16 14:58:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reproduction node polyfill not working
+
+When running this app, an error is thrown because `Buffer` doesn't seem to be polyfilled.
+
+To run the app,
+
+```bash
+pnpm i
+pnpm dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { crypto } from "bitcoinjs-lib";
+</script>
+
+<main>
+  <h1>Using a package with Buffer</h1>
+  <div>{crypto.sha256("abc")}</div>
+</main>
+
+<style>
+</style>
